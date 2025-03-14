0b7463720e5453d1e40ef317e63425630c23935a
Clienttl <highridess@outlook.com>
2025-03-14 17:22:18 -0400

    MACHINEEEE

--- machine.js
+++ machine.js
@@ -1,7 +1,8 @@
+// do not touch, this code is licensed under a proprietary license, and you can face legal trouble if tampered, or made a copy of. it is only to be granted access by the original creator @kyaisw
+// this script is only to be used by members of discord.gg/vdtU9gQQ [machine], will not be useful outside of it.
 (function() {
     'use strict';
-
-    let users = [
+    let _0x123456 = [
         { name: "kyaisw", tag: "[Contributor] " },
         { name: "Zade", tag: "[Leader] " },
         { name: "gok4rt", tag: "[Commander] " },
@@ -11,54 +12,90 @@
         { name: "Bunny 🐰 :)", tag: "[Jr. Commander] " },
         { name: "Lunari", tag: "[Commander] " }
     ];
-
-    let _c = "rainbow-text", _d = "chat-message-sender", _e = "leaderboard-name", _f = "innerText", 
-        _g = "classList", _h = "trim", _i = "includes", _j = "startsWith", _k = "forEach", 
-        _l = "querySelectorAll", _m = "style", _n = "head", _o = "appendChild", _p = "createElement", 
-        _q = "animation", _r = "rainbow", _s = "infinite", _t = "linear", _u = "color", 
-        _v = "red", _w = "orange", _x = "yellow", _y = "green", _z = "blue", 
-        _A = "indigo", _B = "violet", _C = "2s", _E = 1;
-
-    function _F() {
-        document[_l]("." + _d)[_k](_G => {
-            users.forEach(user => {
-                if (_G[_f][_h]() === user.name && !_G[_f][_i](user.tag)) {
-                    _G[_f] = user.tag + _G[_f];
-                    _G[_g].add(_c);
+    let _0xabc123 = "rainbow-text", _0xdef456 = "chat-message-sender", _0xghi789 = "leaderboard-name", _0xjkl012 = "innerText",
+        _0xmnp345 = "classList", _0xrst678 = "trim", _0xuvw901 = "includes", _0xyza23 = "startsWith", _0xbcdef4 = "forEach",
+        _0xfgh567 = "querySelectorAll", _0xijk890 = "style", _0xlmn123 = "head", _0xopq456 = "appendChild", _0xrst789 = "createElement",
+        _0xuvw012 = "animation", _0xyza34 = "rainbow", _0xbcdef5 = "infinite", _0xfgh678 = "linear", _0xijk901 = "color",
+        _0xlmn234 = "red", _0xopq567 = "orange", _0xrst890 = "yellow", _0xuvw123 = "green", _0xyza45 = "blue",
+        _0xbcdef6 = "indigo", _0xfgh789 = "violet", _0xijk012 = "2s", _0xlmn345 = 1;
+    function _0x1a2b3c() {
+        document[_0xfgh567]("." + _0xdef456)[_0xbcdef4](_0x4d5e6f => {
+            _0x123456.forEach(_0x7g8h9i => {
+                if (_0x4d5e6f[_0xjkl012][_0xrst678]() === _0x7g8h9i.name && !_0x4d5e6f[_0xjkl012][_0xuvw901](_0x7g8h9i.tag)) {
+                    _0x4d5e6f[_0xjkl012] = _0x7g8h9i.tag + _0x4d5e6f[_0xjkl012];
+                    _0x4d5e6f[_0xmnp345].add(_0xabc123);
                 }
             });
         });
-
-        document[_l]("." + _e)[_k](_H => {
-            users.forEach(user => {
-                if (_H[_f][_h]()[_j](user.name) && !_H[_f][_i](user.tag)) {
-                    _H[_f] = user.tag + _H[_f];
-                    _H[_g].add(_c);
+        document[_0xfgh567]("." + _0xghi789)[_0xbcdef4](_0x0j1k2l => {
+            _0x123456.forEach(_0x3m4n5o => {
+                if (_0x0j1k2l[_0xjkl012][_0xrst678]()[_0xyza23](_0x3m4n5o.name) && !_0x0j1k2l[_0xjkl012][_0xuvw901](_0x3m4n5o.tag)) {
+                    _0x0j1k2l[_0xjkl012] = _0x3m4n5o.tag + _0x0j1k2l[_0xjkl012];
+                    _0x0j1k2l[_0xmnp345].add(_0xabc123);
                 }
             });
         });
     }
-
     setTimeout(() => {
-        _F();
-        setInterval(_F, _E);
-    }, _E);
-
-    let _I = document[_p](_m);
-    _I.innerHTML = `
-@keyframes ${_r} {
-    0% { ${_u}: ${_v}; }
-    16% { ${_u}: ${_w}; }
-    32% { ${_u}: ${_x}; }
-    48% { ${_u}: ${_y}; }
-    64% { ${_u}: ${_z}; }
-    80% { ${_u}: ${_A}; }
-    100% { ${_u}: ${_B}; }
+        _0x1a2b3c();
+        setInterval(_0x1a2b3c, _0xlmn345);
+    }, _0xlmn345);
+    let _0x6p7q8r = document[_0xrst789](_0xijk890);
+    _0x6p7q8r.innerHTML = `
+@keyframes ${_0xyza34} {
+    0% { ${_0xijk901}: ${_0xlmn234}; }
+    16% { ${_0xijk901}: ${_0xopq567}; }
+    32% { ${_0xijk901}: ${_0xrst890}; }
+    48% { ${_0xijk901}: ${_0xuvw123}; }
+    64% { ${_0xijk901}: ${_0xyza45}; }
+    80% { ${_0xijk901}: ${_0xbcdef6}; }
+    100% { ${_0xijk901}: ${_0xfgh789}; }
 }
-
-.${_c} {
-    ${_q}: ${_r} ${_C} ${_s} ${_t};
+.${_0xabc123} {
+    ${_0xuvw012}: ${_0xyza34} ${_0xijk012} ${_0xbcdef5} ${_0xfgh678};
 }
 `;
-    document[_n][_o](_I);
+    document[_0xlmn123][_0xopq456](_0x6p7q8r);
+    let _0x9s0t1u = false;
+    document.addEventListener('keydown', function(event) {
+        if (event.key === '[') {
+            _0x9s0t1u = !_0x9s0t1u;
+
+            if (_0x9s0t1u) {
+                const _0x2v3w4x = new KeyboardEvent('keydown', {
+                    key: 'd',
+                    code: 'KeyD',
+                    which: 68,
+                    keyCode: 68,
+                    charCode: 0,
+                    bubbles: true,
+                    cancelable: true,
+                    view: window
+                });
+                document.dispatchEvent(_0x2v3w4x);
+            } else {
+                const _0x5y6z7a = new KeyboardEvent('keyup', {
+                    key: 'd',
+                    code: 'KeyD',
+                    which: 68,
+                    keyCode: 68,
+                    charCode: 0,
+                    bubbles: true,
+                    cancelable: true,
+                    view: window
+                });
+                document.dispatchEvent(_0x5y6z7a);
+            }
+        }
+    });
+    document.addEventListener('keyup', function(event){
+        if(event.key === 'd' && _0x9s0t1u){
+            _0x9s0t1u = false;
+        }
+    });
+    document.addEventListener('keydown', function(event){
+        if(event.key === 'd' && _0x9s0t1u){
+            _0x9s0t1u = false;
+        }
+    });
 })();
